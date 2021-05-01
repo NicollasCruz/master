8798ac58f27fd1f037c3642699c86eaffd746964
Nicollas Lopes <collascollas29@gmail.com>
2021-05-01 14:53:02 -0300

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -21,18 +21,30 @@
         }
 
         mysqli_select_db($link, $db_name) or die("erro".mysqli_connect_error());
-        $filtro = 'SELECT * FROM usuario WHERE name="'.$name.'"';
+        $filtro = 'SELECT * FROM usuario WHERE name="'.$name.'" AND password="'.$password.'"';
         $resultado = mysqli_query($link, $filtro);
         $linhas = mysqli_num_rows($resultado);
 
         return $linhas;
     }
 
-    $verify = reciveData();
+    function verify ($result)
+    {
+        if($result == 1)
+        {
+            return 'Login successfully';
+        }
+        else
+        {
+            return 'Login failed';
+        }
+    }
 
-    $result = connectDb($db_host, $db_user, $db_pass, $db_name, $verify->name, $verify->password);
-    
-    echo $result;
+    $data = reciveData();
 
+    $result = connectDb($db_host, $db_user, $db_pass, $db_name, $data->name, $data->password);
     
+    $verify = verify($result);
+
+    echo $verify;
 ?>
