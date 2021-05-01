79af5ca0be198c3121b65baaf144c162a7a56f80
Nicollas Lopes <collascollas29@gmail.com>
2021-05-01 14:48:02 -0300

    Agora sim...

--- backend/login.php
+++ backend/login.php
@@ -21,7 +21,7 @@
         }
 
         mysqli_select_db($link, $db_name) or die("erro".mysqli_connect_error());
-        $filtro = 'SELECT name, password FROM usuario WHERE name="$name" AND password="$password"';
+        $filtro = 'SELECT * FROM usuario WHERE name="'.$name.'"';
         $resultado = mysqli_query($link, $filtro);
         $linhas = mysqli_num_rows($resultado);
 
@@ -31,7 +31,7 @@
     $verify = reciveData();
 
     $result = connectDb($db_host, $db_user, $db_pass, $db_name, $verify->name, $verify->password);
-
+    
     echo $result;
 
     
