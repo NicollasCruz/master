f912fe4b16fefb9789e7c1f006da6f5e54e8545e
Nicollas Lopes <collascollas29@gmail.com>
2021-05-03 23:08:35 -0300

    Adicionando a verificação de caracteres

--- scripts/login.js
+++ scripts/login.js
@@ -3,6 +3,22 @@ function sendLogin()
         let name = document.querySelector("#name-login").value;
         let password = document.querySelector("#password-login").value;
 
+        let forbidenChar = ['!', '@', '#', '$', '%', '&', '*', '(', ')', ' - ', ' _ ', ' = ', ' + ', ' ´ ', ' ` ', ' [ ', ' ] ', ' { ', ' } ', ' ~ ', ' ^ ', ' , ', ' . ', ' / ', ' ? ', ' ; ', ' : ', ' < ', ' > '];
+
+        for(let x = 0; x < name.length; x++)
+        {
+            for(let y = 0; y < forbidenChar.length; y++)
+            {
+                if(name[x] == forbidenChar[y])
+                {
+                    alert('Requisição não enviada devido ao uso do caracter ' + forbidenChar[y]);
+                    document.querySelector("#name-login").value = '';
+                    document.querySelector("#password-login").value = '';
+                    return;
+                }
+            }
+        }
+
         let user = {
                 name: name,
                 password: password
