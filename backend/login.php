7dd9c377b0213245249f7128b4a706c6f0793daf
Nicollas Lopes <collascollas29@gmail.com>
2021-05-03 22:06:43 -0300

    Revert "Fazendo merda ao tentar deixar o bglh orientado a obj"
    This reverts commit 497be0d47d6b3b48ae87864f2d9754c0ba0dc2c0.

--- backend/login.php
+++ backend/login.php
@@ -1,51 +1,50 @@
 <?php
-    class Login
+
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+    $db_name = 'tasklist';
+
+    function reciveData(){
+        $meuPost = file_get_contents("php://input");
+
+        $json = json_decode( $meuPost );
+
+        return $json;
+    }
+
+    function connectDb($db_host, $db_user, $db_pass, $db_name, $name, $password)
     {
-        public string $db_host = 'localhost';
-        public string $db_user = 'root';
-        public string $db_pass = '';
-        public string $db_name = 'tasklist';
-        private string $name;
-        private string $password;
-
-        public function showLogin():string{
-            return $this -> checkUser;
+        $link = mysqli_connect($db_host, $db_user, $db_pass);
+        if(!$link){
+            die("erro".mysqli_connect_error());
         }
 
-        public function setName(string $name):void{
-            $this -> name = $name;
-        }
+        mysqli_select_db($link, $db_name) or die("erro".mysqli_connect_error());
+        $filtro = 'SELECT * FROM usuario WHERE name="'.$name.'" AND password="'.$password.'"';
+        $resultado = mysqli_query($link, $filtro);
+        $linhas = mysqli_num_rows($resultado);
 
-        public function setPassword(string $password):void{
-            $this -> password = $password;
-        }
+        return $linhas;
+    }
 
-        private function connectDb($db_host, $db_user, $db_pass, $db_name):void
+    function verify ($result)
+    {
+        if($result == 1)
         {
-            $link = mysqli_connect($db_host, $db_user, $db_pass);
-            if(!$link)
-                die("erro".mysqli_connect_error());
-            mysqli_select_db($link, $db_name) or die("erro".mysqli_connect_error());
+            return 'Login successfully';
         }
-
-        private function queryDb($name, $password):bool
+        else
         {
-            $conection = connectDb($db_host, $db_user, $db_pass, $db_name);
-            $filtro = 'SELECT * FROM usuario WHERE name="'.$name.'" AND password="'.$password.'"';
-            $resultado = mysqli_query($conection, $filtro);
-            $linhas = mysqli_num_rows($resultado);
-
-            return $linhas;
-        }
-
-        private function verify ($result):string
-        {
-            $result = queryDb($name, $password);
-
-            if($result == 1)
-                $this -> checkUser =  'Login successfully';
-            else
-                $this -> checkUser = 'Login failed';
+            return 'Login failed';
         }
     }
+
+    $data = reciveData();
+
+    $result = connectDb($db_host, $db_user, $db_pass, $db_name, $data->name, $data->password);
+    
+    $verify = verify($result);
+
+    echo $verify;
 ?>
